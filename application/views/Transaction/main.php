4eee729c7e149ee611d2ee028c0da5f12394829a
Rizqal Azizhan B <rizqalazizhan@gmail.com>
2024-01-15 14:26:17 +0700

    update

--- application/views/Transaction/main.php
+++ application/views/Transaction/main.php
@@ -8,6 +8,55 @@
       </ol>
     </section>
 
-    <!-- Main content -->
-    
+ <!-- Main content -->
+ <section class="content">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-gray ">
+            <div class="inner">
+              <img src="img/default.jpg" alt="" style="margin-left:15px;">
+            </div>
+            <a href="#" class="small-box-footer">Request <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-gray">
+            <div class="inner">
+              <img src="img/default.jpg" alt="" style="margin-left:15px;">
+            </div>
+            <a href="#" class="small-box-footer">Request <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-gray">
+            <div class="inner">
+              <img src="img/PU.jpg" alt="" style="margin-left:15px;">
+            </div>
+            <a href="#" class="small-box-footer">Request <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-gray">
+            <div class="inner">
+              <img src="img/default.jpg" alt="" style="margin-left:15px;">
+            </div>
+            <a href="#" class="small-box-footer">Request <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+      <!-- Main row -->
+     
+      <!-- /.row (main row) -->
+
+    </section>
     <!-- /.content -->
