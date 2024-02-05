58c67e6ef408c5429e3ce205163d419d5e692c52
Rizqal Azizhan B <rizqalazizhan@gmail.com>
2024-02-05 11:45:01 +0700

    Update Item Required

--- application/views/User/Role/main.php
+++ application/views/User/Role/main.php
@@ -107,12 +107,12 @@
                         <div class="box-body">
                             <div class="form-group">
                               <label for="text">Nama Role</label>
-                              <input type="text" class="form-control" name="label" placeholder="Nama Role">
+                              <input type="text" class="form-control" name="label" placeholder="Nama Role" required>
                               <p class="text-red"><?php echo form_error('label')?></p>
                             </div>
                             <div class="form-group">
                               <label for="text">Level Role</label>
-                              <input type="number" class="form-control" name="level" placeholder="Level">
+                              <input type="number" class="form-control" name="level" placeholder="Level" required>
                               <p class="text-red"><?php echo form_error('level')?></p>
                             </div>
                     </div>
